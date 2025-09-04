ebb935c71a130fd660f6681866c177cf68a2284f
jtfirek <firekjt@gmail.com>
2025-09-04 00:57:15 -0400

    change compiler version

--- src/BeHYPE.sol
+++ src/BeHYPE.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.24;
 
 import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
