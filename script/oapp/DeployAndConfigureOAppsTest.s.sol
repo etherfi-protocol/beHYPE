6ddcbacbefaaacda23e62c79eb705ce20be7227f
jtfirek <firekjt@gmail.com>
2025-11-05 14:51:22 -0500

    L2 staker audit fixes

--- script/oapp/DeployAndConfigureOAppsTest.s.sol
+++ script/oapp/DeployAndConfigureOAppsTest.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {console} from "forge-std/console.sol";
 import "forge-std/StdJson.sol";
 
 import {UUPSProxy} from "../../src/lib/UUPSProxy.sol";
@@ -10,41 +9,40 @@ import {L1BeHYPEOAppStaker} from "../../src/L1BeHYPEOAppStaker.sol";
 import {L2BeHYPEOAppStaker} from "../../src/L2BeHYPEOAppStaker.sol";
 import "../utils/Utils.sol";
 
-import {OptionsBuilder} from "lib/devtools/packages/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
-import {EnforcedOptionParam} from "lib/devtools/packages/oapp-evm/contracts/oapp/interfaces/IOAppOptionsType3.sol";
 import {ILayerZeroEndpointV2} from "lib/LayerZero-v2/packages/layerzero-v2/evm/protocol/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {SetConfigParam} from "lib/LayerZero-v2/packages/layerzero-v2/evm/protocol/contracts/interfaces/IMessageLibManager.sol";
 import {UlnConfig} from "lib/LayerZero-v2/packages/layerzero-v2/evm/messagelib/contracts/uln/UlnBase.sol";
 
 ///
-/// forge script script/oapp/DeployAndConfigureOAppsTest.s.sol:DeployAndConfigureOAppsTest \
-///   --rpc-url $RPC \
-///   --broadcast \
-///   -vvvv 
+/// Deploys and configures L1BeHYPEOAppStaker (on HyperEVM) and L2BeHYPEOAppStaker (on Scroll)
+/// 
+/// Usage:
+///   forge script script/oapp/DeployAndConfigureOAppsTest.s.sol:DeployAndConfigureOAppsTest \
+///     --rpc-url $RPC \
+///     --broadcast \
+///     -vvvv
 contract DeployAndConfigureOAppsTest is Script, Utils {
     using stdJson for string;
-    using OptionsBuilder for bytes;
 
-    // Deployed proxy addresses
-    address payable public constant HYPEREVM_DEPLOYMENT = payable(0x49B27e2b7b79bF0970CbCe336a395d657F6318d9);
-    address payable public constant SCROLL_DEPLOYMENT= payable(0x0Df88e9A5f5783653364A8Bb9C4525a2dB858bBb);
+    address payable public constant HYPEREVM_DEPLOYMENT = payable(address(0x07B23BB48BDf6C223D727e0979DcE34f3e911b40));
+    address payable public constant SCROLL_DEPLOYMENT = payable(address(0x15Ce935E7721Cee88B4Bc0bb4Dc89bb348Cd3A4A));
     
     address payable public l1BeHYPEOAppStaker;
     address payable public l2BeHYPEOAppStaker;
-
-    EnforcedOptionParam[] public enforcedOptions;
 
     function run() external {
         string memory cfg = _readConfig();
         bool isScroll = (block.chainid == 534352);
         uint128 enforceGas = uint128(500_000);
+        uint128 lzReceiveGasLimit = uint128(200_000);
         address owner = msg.sender;
 
         vm.startBroadcast();
 
         if (isScroll) {
-            L2BeHYPEOAppStaker depositImpl = new L2BeHYPEOAppStaker(cfg.readAddress(".layerZero.scroll.endpoint"));
-            l2BeHYPEOAppStaker = _deployOAppDeposit(address(depositImpl), owner, enforceGas);
+            L2BeHYPEOAppStaker l2Impl = new L2BeHYPEOAppStaker(cfg.readAddress(".layerZero.scroll.endpoint"));
+            l2BeHYPEOAppStaker = _deployL2OAppStaker(address(l2Impl), owner, enforceGas, lzReceiveGasLimit);
+            
             if (l2BeHYPEOAppStaker != address(SCROLL_DEPLOYMENT)) {
                 revert("L2BeHYPEOAppStaker is not the same as SCROLL_DEPLOYMENT");
             }
@@ -52,6 +50,7 @@ contract DeployAndConfigureOAppsTest is Script, Utils {
             address scrollEndpoint = cfg.readAddress(".layerZero.scroll.endpoint");
             uint32 dstEid = uint32(cfg.readUint(".layerZero.hyperEVM.eid"));
             L2BeHYPEOAppStaker(l2BeHYPEOAppStaker).setPeer(dstEid, bytes32(uint256(uint160(address(HYPEREVM_DEPLOYMENT)))));
+            
             _setDVN(
                 dstEid,
                 scrollEndpoint,
@@ -63,8 +62,9 @@ contract DeployAndConfigureOAppsTest is Script, Utils {
             );
             
         } else {
-            L1BeHYPEOAppStaker senderImpl = new L1BeHYPEOAppStaker(cfg.readAddress(".layerZero.hyperEVM.endpoint"));
-            l1BeHYPEOAppStaker = _deployOAppStaker(address(senderImpl), owner);
+            L1BeHYPEOAppStaker l1Impl = new L1BeHYPEOAppStaker(cfg.readAddress(".layerZero.hyperEVM.endpoint"));
+            l1BeHYPEOAppStaker = _deployL1OAppStaker(address(l1Impl), owner);
+            
             if (l1BeHYPEOAppStaker != address(HYPEREVM_DEPLOYMENT)) {
                 revert("L1BeHYPEOAppStaker is not the same as HYPEREVM_DEPLOYMENT");
             }
@@ -72,6 +72,7 @@ contract DeployAndConfigureOAppsTest is Script, Utils {
             address hyperEVMEndpoint = cfg.readAddress(".layerZero.hyperEVM.endpoint");
             uint32 dstEid = uint32(cfg.readUint(".layerZero.scroll.eid"));
             L1BeHYPEOAppStaker(l1BeHYPEOAppStaker).setPeer(dstEid, bytes32(uint256(uint160(address(SCROLL_DEPLOYMENT)))));
+            
             _setDVN(
                 dstEid,
                 hyperEVMEndpoint,
@@ -86,24 +87,24 @@ contract DeployAndConfigureOAppsTest is Script, Utils {
         vm.stopBroadcast();
     }
 
-    function _deployOAppDeposit(address impl, address owner, uint128 enforceOptions) internal returns (address payable) {
-        address proxy = deployWithCreate3(
-            abi.encodePacked(
-                type(UUPSProxy).creationCode,
-                abi.encode(impl, abi.encodeWithSelector(L2BeHYPEOAppStaker.initialize.selector, owner, enforceOptions))
-            ),
-            keccak256(bytes("L2BeHYPEOAppStakerTest"))
-        );
-        return payable(proxy);
-    }
-
-    function _deployOAppStaker(address impl, address owner) internal returns (address payable) {
+    function _deployL1OAppStaker(address impl, address owner) internal returns (address payable) {
         address proxy = deployWithCreate3(
             abi.encodePacked(
                 type(UUPSProxy).creationCode,
                 abi.encode(impl, abi.encodeWithSelector(L1BeHYPEOAppStaker.initialize.selector, owner))
             ),
-            keccak256(bytes("L1BeHYPEOAppStakerTest"))
+            keccak256(bytes("L1BeHYPEOAppStakerTest2"))
+        );
+        return payable(proxy);
+    }
+
+    function _deployL2OAppStaker(address impl, address owner, uint128 enforceOptions, uint128 lzReceiveGasLimit) internal returns (address payable) {
+        address proxy = deployWithCreate3(
+            abi.encodePacked(
+                type(UUPSProxy).creationCode,
+                abi.encode(impl, abi.encodeWithSelector(L2BeHYPEOAppStaker.initialize.selector, owner, enforceOptions, lzReceiveGasLimit))
+            ),
+            keccak256(bytes("L2BeHYPEOAppStakerTest2"))
         );
         return payable(proxy);
     }
--- src/L1BeHYPEOAppStaker.sol
+++ src/L1BeHYPEOAppStaker.sol
@@ -7,9 +7,11 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {MessagingFee, MessagingReceipt} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+import {IOAppComposer} from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppComposer.sol";
 import {IStakingCore} from "./interfaces/IStakingCore.sol";
 import {BeHYPEOFTAdapter} from "./BeHYPEOFTAdapter.sol";
 import {IOFT, SendParam} from "lib/devtools/packages/oft-evm/contracts/interfaces/IOFT.sol";
+import {OFTComposeMsgCodec} from "lib/devtools/packages/oft-evm/contracts/libs/OFTComposeMsgCodec.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IWHYPE} from "./interfaces/IWHYPE.sol";
@@ -18,7 +20,8 @@ contract L1BeHYPEOAppStaker is
     Initializable,
     UUPSUpgradeable,
     OwnableUpgradeable,
-    OAppUpgradeable
+    OAppUpgradeable,
+    IOAppComposer
 {
     using OptionsBuilder for bytes;
     using SafeERC20 for IERC20;
@@ -38,34 +41,76 @@ contract L1BeHYPEOAppStaker is
         __UUPSUpgradeable_init();
     }
 
-    function _lzReceive(
-        Origin calldata _origin,
+    /**
+     * @notice Receives composed messages from L2BeHYPEOAppStaker after token transfer completes
+     * @dev This function is called by the LayerZero endpoint after the OFT transfer completes
+     * @param _from The address that initiated the compose (the OFT contract)
+     * @param _message The composed message encoded by OFTComposeMsgCodec
+     */
+    function lzCompose(
+        address _from,
         bytes32 /*_guid*/,
         bytes calldata _message,
         address /*_executor*/,
         bytes calldata /*_extraData*/
-    ) internal override {
-        (uint256 amountReceived, address receiver) = abi.decode(_message, (uint256, address));
+    ) external payable override {
+        if (address(endpoint) != msg.sender) revert OnlyEndpoint(msg.sender);
+        
+        uint256 amountReceived = OFTComposeMsgCodec.amountLD(_message);
+        bytes memory originalComposeMsg = OFTComposeMsgCodec.composeMsg(_message);
+        
+        address receiver = abi.decode(originalComposeMsg, (address));
 
         WHYPE.withdraw(amountReceived);
         STAKING_CORE.stake{value: amountReceived}("");
         uint256 beHYPEStaked = BEHYPE.balanceOf(address(this));
-        uint256 minAmount = beHYPEStaked - (beHYPEStaked / 10000); 
+        uint256 beHYPEStakedWithoutDust = _removeDust(beHYPEStaked);
 
         SendParam memory sendParam = SendParam({
-            dstEid: _origin.srcEid,
+            dstEid: OFTComposeMsgCodec.srcEid(_message),
             to: bytes32(uint256(uint160(receiver))),
-            amountLD: beHYPEStaked,
-            minAmountLD: minAmount,
+            amountLD: beHYPEStakedWithoutDust,
+            minAmountLD: beHYPEStakedWithoutDust,
             extraOptions: "",
             composeMsg: "",
             oftCmd: ""
         });
         MessagingFee memory fee = BEHYPE_OFT_ADAPTER.quoteSend(sendParam, false);
 
-        BEHYPE.approve(address(BEHYPE_OFT_ADAPTER), beHYPEStaked);
+        BEHYPE.approve(address(BEHYPE_OFT_ADAPTER), beHYPEStakedWithoutDust);
         BEHYPE_OFT_ADAPTER.send{value: fee.nativeFee}(sendParam, fee, address(this));
     }
+
+    /**
+     * @notice Sweeps accumulated dust beHYPE tokens to the contract owner
+     * @dev This function recovers dust funds that accumulate due to the 6 decimal shared precision
+     *      limitation between HypeEVM and Scroll chains. When beHYPE is staked on HypeEVM, the
+     *      amount received from STAKING_CORE is computed dynamically and has precision beyond
+     *      6 decimals. During cross-chain bridging, only 6 decimal precision is preserved, leaving
+     *      dust (< 10^12 wei) locked in this contract.
+     */
+    function sweepDust() external onlyOwner {
+        BEHYPE.safeTransfer(owner(), BEHYPE.balanceOf(address(this)));
+    }
+
+    /**
+     * @dev Removes dust from the given local decimal amount using the OFT's decimalConversionRate
+     * @param _amountLD The amount in local decimals
+     * @return The amount after removing dust
+     * @dev matches the calculation used in OFTCore: (amountLD / conversionRate) * conversionRate
+     */
+    function _removeDust(uint256 _amountLD) internal pure returns (uint256) {
+        uint256 conversionRate = 1e12; // 10^12 (18 decimals - 6 shared decimals)
+        return (_amountLD / conversionRate) * conversionRate;
+    }
+
+    function _lzReceive(
+        Origin calldata /*_origin*/,
+        bytes32 /*_guid*/,
+        bytes calldata /*_message*/,
+        address /*_executor*/,
+        bytes calldata /*_extraData*/
+    ) internal override {}
 
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
